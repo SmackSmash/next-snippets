261879c317e6d49ac84e6c76a8471b58ea6a88a2
Dan Moffat <danmoffat@gmail.com>
2025-08-05 22:19:46 +0100

    Little styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} container mx-auto font-sans antialiased`}
+      >
         {children}
       </body>
     </html>
