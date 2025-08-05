b1d6ce09bf3a28ab0f3bbb56facc0b0df30abe41
Dan Moffat <danmoffat@gmail.com>
2025-08-05 22:17:35 +0100

    Added a submit btton

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -15,6 +15,12 @@ export default function SnippetCreatePage() {
         id='code'
         className='min-h-64 rounded bg-zinc-200 p-2 font-mono text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
       ></textarea>
+      <button
+        type='submit'
+        className='cursor-pointer rounded bg-zinc-800 p-4 text-zinc-50 dark:bg-zinc-200 dark:text-zinc-900'
+      >
+        Add Snippet
+      </button>
     </form>
   );
 }
