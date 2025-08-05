1b730807541d8f7b15500dd97ab4ad0fc802764d
Dan Moffat <danmoffat@gmail.com>
2025-08-05 21:45:43 +0100

    Added a form plus db client

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,11 @@
+export default function SnippetCreatePage() {
+  return (
+    <form action='' className='m-4 flex flex-col gap-4 rounded bg-zinc-900 p-4'>
+      <h3 className='text-2xl font-bold'>Create a Snippet</h3>
+      <label htmlFor='title'>Title</label>
+      <input type='text' name='title' id='title' className='rounded bg-zinc-200' />
+      <label htmlFor='code'>Code</label>
+      <textarea name='code' id='code' className='rounded bg-zinc-200'></textarea>
+    </form>
+  );
+}
