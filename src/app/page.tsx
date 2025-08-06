917ec1f870be88abcb6fe4dfe9bee5450fdf0944
Dan Moffat <danmoffat@gmail.com>
2025-08-06 17:19:07 +0100

    Added 'new snippet' link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import { prisma } from '@/db';
 import CodeBlock from '@/components/code-block/code-block';
 import ShowSnippet from '@/components/show-snippet';
+import Link from 'next/link';
 
 export default async function HomePage() {
   const snippets = await prisma.snippet.findMany();
 
   return (
     <>
-      <h1 className='mb-2 text-2xl font-bold'>Snippets</h1>
+      <div className='flex items-center justify-between'>
+        <h1 className='mb-2 text-2xl font-bold'>Snippets</h1>
+        <Link
+          href='/snippets/new'
+          className='cursor-pointer rounded bg-zinc-800 px-4 py-2 text-zinc-50 dark:bg-zinc-200 dark:text-zinc-900'
+        >
+          New Snippet
+        </Link>
+      </div>
       <div className='flex flex-col gap-8'>
         {snippets.map(({ id, title, code }) => (
           <ShowSnippet key={id} id={id} title={title} code={code} />
--- src/components/show-snippet.tsx
+++ src/components/show-snippet.tsx
@@ -12,7 +12,7 @@ export default function ShowSnippet({ id, title, code }: ShowSnippetProps) {
     <div className='flex flex-col gap-2'>
       <div className='flex items-center justify-between'>
         <h2 className='text-xl'>{title}</h2>
-        {id && <Link href={`/snippets/${id}`}>View Snippet</Link>}
+        {id && <Link href={`/snippets/${id}`}>View Snippet →</Link>}
       </div>
       <CodeBlock lang='ts'>{code}</CodeBlock>
     </div>
