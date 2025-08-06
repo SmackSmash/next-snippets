1aac279e110bed345c35a882f3dae2b1240e9509
Dan Moffat <danmoffat@gmail.com>
2025-08-06 22:22:59 +0100

    Added codemirror editor

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
+import { EditorView, basicSetup } from 'codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { nord } from '@fsegurai/codemirror-theme-nord';
+
 type EditSnippetFormProps = {
   snippet: {
     id: number;
@@ -9,6 +14,20 @@ type EditSnippetFormProps = {
 };
 
 export default function EditSnippetForm({ snippet: { id, title, code } }: EditSnippetFormProps) {
+  const editor = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let view = new EditorView({
+      doc: code,
+      extensions: [basicSetup, javascript(), nord],
+      parent: editor.current as HTMLDivElement
+    });
+
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
   return (
     <form className='flex flex-col gap-4'>
       <h3 className='text-2xl font-bold'>Edit Snippet #{id}</h3>
@@ -21,12 +40,13 @@ export default function EditSnippetForm({ snippet: { id, title, code } }: EditSn
         className='rounded bg-zinc-200 p-2 text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
       />
       <label htmlFor='code'>Code</label>
-      <textarea
+      {/* <textarea
         name='code'
         id='code'
         defaultValue={code}
         className='min-h-64 rounded bg-zinc-200 p-2 font-mono text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
-      ></textarea>
+      ></textarea> */}
+      <div ref={editor}></div>
       <button
         type='submit'
         onClick={() => alert('Oi!')}
