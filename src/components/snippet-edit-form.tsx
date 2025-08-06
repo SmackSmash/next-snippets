44daee32c442e835f2131ad3d4aa5137d9b0cf6e
Dan Moffat <danmoffat@gmail.com>
2025-08-07 00:29:41 +0100

    Synced original textarea with editor. Can I still use a server action?

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -53,13 +53,12 @@ export default function EditSnippetForm({ snippet: { id, title, code } }: EditSn
         className='rounded bg-zinc-200 p-2 text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
       />
       <label htmlFor='code'>Code</label>
-      {/* <textarea
+      <textarea
         name='code'
         id='code'
-        defaultValue={code}
+        value={editorCode}
         className='min-h-64 rounded bg-zinc-200 p-2 font-mono text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
-      ></textarea> */}
-      <div>{editorCode}</div>
+      ></textarea>
       <div ref={editor}></div>
       <button
         type='submit'
