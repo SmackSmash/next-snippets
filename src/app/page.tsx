bb78827b329ce296e1274d4b463fcb9ecc75947d
Dan Moffat <danmoffat@gmail.com>
2025-08-06 01:15:17 +0100

    Display snippets on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { prisma } from '@/db';
+
+export default async function HomePage() {
+  const snippets = await prisma.snippet.findMany();
+
+  console.log(snippets);
+
+  return (
+    <div className='m-4 flex flex-col gap-4 rounded bg-zinc-100 p-4 dark:bg-zinc-900'>
+      <h1 className='mb-2 text-2xl font-bold'>Snippets</h1>
+      <div className='flex flex-col gap-8'>
+        {snippets.map(({ id, title, code }) => (
+          <div key={id} className='flex flex-col gap-2'>
+            <h2 className='text-xl'>{title}</h2>
+            <p className='font-mono'>{code}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
