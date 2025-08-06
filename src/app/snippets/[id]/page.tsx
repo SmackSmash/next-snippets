3552d7cbef8798a0b14a8c04aa4ead3afb18c9a8
Dan Moffat <danmoffat@gmail.com>
2025-08-06 02:58:17 +0100

    A better guard clause

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -10,16 +10,14 @@ export default async function ShowSnippetPage({ params }: ShowSnippetPageProps) 
   const { id } = await params;
   const snippet = await prisma.snippet.findUnique({ where: { id: Number(id) } });
 
-  if (!snippet) {
-    return notFound();
-  } else {
-    const { title, code } = snippet;
+  if (!snippet) return notFound();
 
-    return (
-      <div className='m-4 flex flex-col gap-4 rounded bg-zinc-100 p-4 dark:bg-zinc-900'>
-        <h1 className='mb-2 text-2xl font-bold'>Snippet {id}</h1>
-        <ShowSnippet title={title} code={code} />
-      </div>
-    );
-  }
+  const { title, code } = snippet;
+
+  return (
+    <div className='m-4 flex flex-col gap-4 rounded bg-zinc-100 p-4 dark:bg-zinc-900'>
+      <h1 className='mb-2 text-2xl font-bold'>Snippet {id}</h1>
+      <ShowSnippet title={title} code={code} />
+    </div>
+  );
 }
