fb9341b9a011baf85c37435102858d47dbbedeef
Dan Moffat <danmoffat@gmail.com>
2025-08-06 02:31:42 +0100

    TS error fixed with guard clause

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,3 +1,25 @@
-export default function ShowSnippetPage() {
-  return <div>Hello there!</div>;
+import { prisma } from '@/db';
+import CodeBlock from '@/components/code-block/code-block';
+
+type ShowSnippetPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function ShowSnippetPage({ params }: ShowSnippetPageProps) {
+  const { id } = await params;
+  const snippet = await prisma.snippet.findUnique({ where: { id: Number(id) } });
+
+  if (snippet) {
+    const { title, code } = snippet;
+
+    return (
+      <div className='m-4 flex flex-col gap-4 rounded bg-zinc-100 p-4 dark:bg-zinc-900'>
+        <h1 className='mb-2 text-2xl font-bold'>Snippet {id}</h1>
+        <div className='flex flex-col gap-2'>
+          <h2 className='text-xl'>{title}</h2>
+          <CodeBlock lang='ts'>{code}</CodeBlock>
+        </div>
+      </div>
+    );
+  }
 }
