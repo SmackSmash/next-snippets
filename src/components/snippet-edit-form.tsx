0af0748f641a89348dadda4933e6778e0ecf41fd
Dan Moffat <danmoffat@gmail.com>
2025-08-06 21:24:40 +0100

    Moved edit form to client component

---
+++ src/components/snippet-edit-form.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import type { FC } from 'react';
+
+const EditSnippetForm: FC<{
+  snippet: {
+    id: number;
+    title: string;
+    code: string;
+  };
+}> = ({ snippet: { id, title, code } }) => {
+  return (
+    <form className='flex flex-col gap-4'>
+      <h3 className='text-2xl font-bold'>Edit Snippet #{id}</h3>
+      <label htmlFor='title'>Title</label>
+      <input
+        type='text'
+        name='title'
+        id='title'
+        defaultValue={title}
+        className='rounded bg-zinc-200 p-2 text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
+      />
+      <label htmlFor='code'>Code</label>
+      <textarea
+        name='code'
+        id='code'
+        defaultValue={code}
+        className='min-h-64 rounded bg-zinc-200 p-2 font-mono text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
+      ></textarea>
+      <button
+        type='submit'
+        onClick={() => alert('Oi!')}
+        className='cursor-pointer rounded bg-zinc-800 p-4 text-zinc-50 dark:bg-zinc-200 dark:text-zinc-900'
+      >
+        Edit Snippet
+      </button>
+    </form>
+  );
+};
+
+export default EditSnippetForm;
