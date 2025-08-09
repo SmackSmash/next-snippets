ffde5b006c526578cd9ee9cc3fa771f60cb9eed7
Dan Moffat <danmoffat@gmail.com>
2025-08-10 00:16:28 +0100

    Added pending state

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -6,6 +6,7 @@ import { createSnippet } from '@/actions';
 export default function CreateSnippetPage() {
   // useActionState can allow us to communicate back from the server
   // function to the component in a way that does not require js
+  // Basically it's PHP, but don't tell anyone I said that
   const [state, action, pending] = useActionState(createSnippet, {
     message: 'Nothing to see here'
   });
@@ -30,7 +31,7 @@ export default function CreateSnippetPage() {
         id='code'
         className='min-h-64 rounded bg-zinc-200 p-2 font-mono text-zinc-900 outline-0 focus-visible:outline-2 dark:bg-zinc-800 dark:text-zinc-50'
       ></textarea>
-      {state.message}
+      {pending ? 'Loading...' : state.message}
       <button
         type='submit'
         className='cursor-pointer rounded bg-zinc-800 p-4 text-zinc-50 dark:bg-zinc-200 dark:text-zinc-900'
